7f4c1f0596de29c56fe756c0732c65d844e48a05
Kamal Preet Kaur <twinklehitech@gmail.com>
2022-11-03 14:39:22 +0530

    code fixed by m2

--- prime.js
+++ prime.js
@@ -1,13 +1,19 @@
+@@ -1,12 +1,12 @@
+let number=13
+
 function checkprime(number){
-    let count=0;
-    for(let i=0;i<number;i++){
-        if(number%i==1){
-            count++
+    let factors=0;
+    for(let i=1;i<=number;i++){
+        if(number%i==0){
+           
+            factors++;
         }
     }
-    if(count==3){
+  
+    if(factors==2){
         console.log("prime")
     }else{
         console.log("not prime")
-    }
+
 }
+ 
